0108a8d753157a80fa5c5c130f900e9a3182e6c3
OrkhanMusayev <mussayevorkhan@gmail.com>
2020-05-08 15:27:48 -0400

    add LogCall.java

---
+++ src/day_1/LogCall.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class LogCall {
+
+    public static void main(String[] args) {
+
+        // users are able to call through slack
+        System.out.println("Hello");
+    }
+}
